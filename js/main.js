327116a4abc5990821ddac78bdddf173e997e8be
Immolin <serebrena@mail.ru>
2022-06-10 18:46:03 +1000

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0,
 const shuffle = (array) => {
   const result = [...array];
   for (let i = result.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
+    const j = Math.floor(Math.random() * (i + 1));
     [result[i], result[j]] = [result[j], result[i]];
   }
   return result;
@@ -125,3 +125,5 @@ const createAdvert = function(value, index) {
 };
 
 const adverts = Array.from({length: 10}, createAdvert);
+
+console.log(adverts);
