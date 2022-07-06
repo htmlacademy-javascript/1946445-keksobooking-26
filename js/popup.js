ce47435818da2386e670afe9653f8f5a8c943743
Immolin <serebrena@mail.ru>
2022-07-06 18:18:39 +1000

    Отрисуй меня полностью (часть 1)

--- js/popup.js
+++ js/popup.js
@@ -1,49 +1,61 @@
 import {createAdverts} from './create-data.js';
 
+const typeObject = {
+  flat: 'Квартира',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+  hotel: 'Отель'
+};
+
+const getRoomNumber = (quantity) => {
+  if (quantity === 1) {
+    return 'комната';
+  } else if (quantity >= 2 && quantity <=4) {
+    return 'комнаты';
+  } else {
+    return 'комнат';
+  }
+};
+
 
 const userDialog = document.querySelector('.map__canvas');
 const addTemplate = document.querySelector('#card').content.querySelector('.popup');
-
 const addNew = createAdverts();
-
 const userFragment = document.createDocumentFragment();
 
-const typeObject = {
-  Квартира: 'flat',
-  Бунгало: 'bungalow',
-  Дом: 'house',
-  Дворец : 'palace',
-  Отель: 'hotel'
-};
 
 addNew.forEach(({author, offer}) => {
   const addElement = addTemplate.cloneNode(true);
+  const featureContainer = addElement.querySelector('.popup__features');
+  const featureList = featureContainer.querySelectorAll('.popup__feature');
+  const photosList = addElement.querySelector('.popup__photos');
+  const photosItem = photosList.querySelector('.popup__photo');
+  const addElementContent = addElement.children;
+
   addElement.querySelector('.popup__title').textContent = offer.title;
   addElement.querySelector('.popup__text--address').textContent = offer.address;
   addElement.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
-  addElement.querySelector('.popup__type').textContent = Object.keys(typeObject).find((key) => typeObject[key] === offer.type);
-  addElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
+  addElement.querySelector('.popup__type').textContent = typeObject[offer.type];
+  addElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} ${getRoomNumber(offer.rooms)} для ${offer.guests} ${(offer.guests > 1) ? 'гостей' : 'гостя'}`;
   addElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
-  const featureContainer = addElement.querySelector('.popup__features');
-  const featureList = featureContainer.querySelectorAll('.popup__feature');
-  const featureModifier = offer.features.map((feature) => `popup__feature--${feature}`);
+  addElement.querySelector('.popup__description').textContent = offer.description;
+  addElement.querySelector('.popup__avatar').src = author.avatar;
+
   featureList.forEach((featureItem) => {
-    const modifier = featureItem.classList[1];
-    if (!featureModifier.includes(modifier)) {
+    const classContains = offer.features.some((feature) => featureItem.classList.contains(`popup__feature--${feature}`));
+    if (!classContains) {
       featureItem.remove();
     }
   });
-  addElement.querySelector('.popup__description').textContent = offer.description;
-  const photosList = addElement.querySelector('.popup__photos');
-  const photosItem = photosList.querySelector('.popup__photo');
+
   offer.photos.forEach((photo) => {
     const photoNew = photosItem.cloneNode(true);
     photoNew.src = photo;
     photosList.append(photoNew);
   });
   photosItem.classList.add('hidden');
-  addElement.querySelector('.popup__avatar').src = author.avatar;
-  const addElementContent = addElement.children;
+
   for (let i = 0; i < addElementContent.length; i++) {
     if (addElementContent[i].textContent === '') {
       addElementContent[i].classList.add('hidden');
@@ -52,7 +64,7 @@ addNew.forEach(({author, offer}) => {
   userFragment.append(addElement);
 });
 
-
 const finalDialog = userDialog.append(userFragment);
 
 export {finalDialog};
+
