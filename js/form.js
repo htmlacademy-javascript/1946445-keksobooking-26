007f3ce4713ce309d475343780398aba1cba24bd
Immolin <serebrena@mail.ru>
2022-07-12 17:13:39 +1000

    Правда или действие (часть 1)

--- js/form.js
+++ js/form.js
@@ -5,11 +5,14 @@ const formSlider = formNew.querySelector('.ad-form__slider');
 const formFiltersComponents = formFilters.children;
 const roomNumber = formNew.querySelector('#room_number');
 const roomCapacity = formNew.querySelector('#capacity');
+const roomType = formNew.querySelector('#type');
+const roomPrice = formNew.querySelector('#price');
+const checkin = formNew.querySelector('#timein');
+const checkout = formNew.querySelector('#timeout');
 
 const pristine = new Pristine(formNew, {
   classTo: 'ad-form__element',
   errorClass: 'ad-form__element--invalid',
-  successClass: 'ad-form__element--valid',
   errorTextTag: 'span',
   errorTextParent: 'ad-form__element',
   errorTextClass: 'ad-form__error-text',
@@ -22,10 +25,38 @@ const roomNumberCapacityCorrelation = {
   '100': '0'
 };
 
+const roomTypePrice = {
+  'bungalow': 0,
+  'flat': 1000,
+  'hotel': 3000,
+  'house': 5000,
+  'palace': 10000
+};
+
+const roomMaxPrice = 100000;
+
 const validateCapacity = (value) => roomNumberCapacityCorrelation[roomNumber.value].includes(value);
 pristine.addValidator(roomCapacity, validateCapacity, 'Некорректное соотношение комнат и гостей');
 formNew.addEventListener('change', () => {
   pristine.validate(roomCapacity);
+});
+
+roomPrice.placeholder = 1000;
+const setRoomMinPrice = () => roomTypePrice[roomType.value];
+roomType.addEventListener('change', () => {
+  roomPrice.min = setRoomMinPrice();
+  roomPrice.placeholder = setRoomMinPrice();
+});
+
+const validateRoomPrice = (value) => value >= setRoomMinPrice() && value <= roomMaxPrice;
+const validateRoomPriceErrorMessage = `Цена не может быть ниже ${setRoomMinPrice()}руб и выше ${roomMaxPrice}руб`;
+pristine.addValidator(roomPrice, validateRoomPrice, validateRoomPriceErrorMessage);
+
+checkin.addEventListener('change', () => {
+  checkout.value = checkin.value;
+});
+checkout.addEventListener('change', () => {
+  checkin.value = checkout.value;
 });
 
 formNew.addEventListener('submit', (evt) => {
