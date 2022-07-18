76e41a6684ae41a4b52aef87b4e3ba3753a7de76
Immolin <serebrena@mail.ru>
2022-07-19 02:22:59 +1000

    Помощь друга

--- js/form.js
+++ js/form.js
@@ -98,7 +98,7 @@ roomType.addEventListener('change', () => {
 });
 
 roomPrice.addEventListener('change', () => {
-  sliderElement.noUiSlider.set(roomPrice.value);
+  sliderElement.noUiSlider.set(roomPrice.value, null);
 });
 
 setValidator();
