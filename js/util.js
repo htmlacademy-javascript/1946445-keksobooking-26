dcc194cec0075ec7291dc1d59aba3f9a4c982f3b
Immolin <serebrena@mail.ru>
2022-07-24 02:54:23 +1000

    Надо подкачаться

--- js/util.js
+++ js/util.js
@@ -2,41 +2,6 @@ const ALERT_SHOW_TIME = 5000;
 const GET_DATA_SERVER = 'https://26.javascript.pages.academy/keksobooking/data';
 const SEND_DATA_SERVER = 'https://26.javascript.pages.academy/keksobooking';
 
-const getRandomPositiveInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-const getRandomPositiveFloat = (a, b, digits = 5) => {
-  const lower = Math.min(Math.abs(a), Math.abs(b));
-  const upper = Math.max(Math.abs(a), Math.abs(b));
-  const result = Math.random() * (upper - lower) + lower;
-  return +result.toFixed(digits);
-};
-
-const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
-
-const shuffle = (array) => {
-  const result = [...array];
-  for (let i = result.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [result[i], result[j]] = [result[j], result[i]];
-  }
-  return result;
-};
-
-const getRandomArray = (array) => shuffle(array).slice(0, getRandomPositiveInteger(0, array.length));
-
-const addLeadingZeros = (value, size) => {
-  let result = `${value}`;
-  while (result.length < size) {
-    result = `0${result}`;
-  }
-  return result;
-};
-
 const showAlert = (message) => {
   const alertContainer = document.createElement('div');
   alertContainer.style.zIndex = '100';
@@ -58,4 +23,4 @@ const showAlert = (message) => {
   }, ALERT_SHOW_TIME);
 };
 
-export {getRandomPositiveInteger, getRandomPositiveFloat, getRandomArrayElement, shuffle, getRandomArray, addLeadingZeros, showAlert, GET_DATA_SERVER, SEND_DATA_SERVER};
+export {showAlert, GET_DATA_SERVER, SEND_DATA_SERVER};
